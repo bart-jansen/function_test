ba99acc8dc6dd88a1fdd2a2729203a42bb2439fd
Bart Jansen <bajansen@microsoft.com>
2019-03-08 11:21:59 +0100

    initial commit

---
+++ AzureFunctions/serveChallenge/index.js
@@ -0,0 +1,61 @@
+
+const KeyVault = require('azure-keyvault');
+const msRestAzure = require('ms-rest-azure');
+
+module.exports = function (context, req) {
+    if(context && context.bindingData && context.bindingData.code && process.env.KEYVAULT_NAME) {
+        //replace illegal chars for secret name in kv
+        let secretName = context.bindingData.code.replace(/([^a-z0-9-]+)/gi, '');
+
+        context.log(`Checking for ACME challenge response at '${secretName}'...`);
+
+        msRestAzure.loginWithAppServiceMSI({resource: 'https://vault.azure.net'}).then(credentials => {
+            const keyVaultClient = new KeyVault.KeyVaultClient(credentials);
+            const vaultUri = `https://${process.env.KEYVAULT_NAME}.vault.azure.net/`;
+            
+            keyVaultClient.getSecret(vaultUri, secretName, "").then(secretData => {
+                context.log(`ACME challenge response file '${secretName}' read successfully.`);
+                context.log(secretData.value);
+
+                context.res = {
+                    status: 200,
+                    headers: { "Content-Type": "text/plain" },
+                    body: secretData.value
+                };
+
+                context.done();
+            }).catch(err => {
+                context.log.error(err);
+
+                context.res = {
+                    status: 404,
+                    headers: { "Content-Type": "text/plain" },
+                    body: 'Error getting secret'
+                };
+        
+                context.done();
+            });
+
+        }).catch(err => {
+            context.log.error(err);
+
+            context.res = {
+                status: 404,
+                headers: { "Content-Type": "text/plain" },
+                body: 'Error logging in with MSI'
+            };
+    
+            context.done();
+        })
+    }
+    else {
+        context.log('No challenge code supplied or keyvault not set up in env variables');
+        context.res = {
+            status: 404,
+            headers: { "Content-Type": "text/plain" },
+            body: 'No challenge code supplied or keyvault not set up in env variables'
+        };
+
+        context.done();
+    }
+};

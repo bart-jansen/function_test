75141dc1198ee1d0d55c1d5dc395102e21d7f42a
Bart Jansen <bajansen@microsoft.com>
2019-03-08 14:11:51 +0100

    simplified fn

--- serveChallenge/index.js
+++ serveChallenge/index.js
@@ -2,60 +2,19 @@
 const KeyVault = require('azure-keyvault');
 const msRestAzure = require('ms-rest-azure');
 
-module.exports = function (context, req) {
-    if(context && context.bindingData && context.bindingData.code && process.env.KEYVAULT_NAME) {
-        //replace illegal chars for secret name in kv
-        let secretName = context.bindingData.code.replace(/([^a-z0-9-]+)/gi, '');
+module.exports = async function (context, req) {
+    context.log('JavaScript HTTP trigger function processed a request.');
 
-        context.log(`Checking for ACME challenge response at '${secretName}'...`);
-
-        msRestAzure.loginWithAppServiceMSI({resource: 'https://vault.azure.net'}).then(credentials => {
-            const keyVaultClient = new KeyVault.KeyVaultClient(credentials);
-            const vaultUri = `https://${process.env.KEYVAULT_NAME}.vault.azure.net/`;
-            
-            keyVaultClient.getSecret(vaultUri, secretName, "").then(secretData => {
-                context.log(`ACME challenge response file '${secretName}' read successfully.`);
-                context.log(secretData.value);
-
-                context.res = {
-                    status: 200,
-                    headers: { "Content-Type": "text/plain" },
-                    body: secretData.value
-                };
-
-                context.done();
-            }).catch(err => {
-                context.log.error(err);
-
-                context.res = {
-                    status: 404,
-                    headers: { "Content-Type": "text/plain" },
-                    body: 'Error getting secret'
-                };
-        
-                context.done();
-            });
-
-        }).catch(err => {
-            context.log.error(err);
-
-            context.res = {
-                status: 404,
-                headers: { "Content-Type": "text/plain" },
-                body: 'Error logging in with MSI'
-            };
-    
-            context.done();
-        })
+    if (req.query.name || (req.body && req.body.name)) {
+        context.res = {
+            // status: 200, /* Defaults to 200 */
+            body: "Hello " + (req.query.name || req.body.name)
+        };
     }
     else {
-        context.log('No challenge code supplied or keyvault not set up in env variables');
         context.res = {
-            status: 404,
-            headers: { "Content-Type": "text/plain" },
-            body: 'No challenge code supplied or keyvault not set up in env variables'
+            status: 400,
+            body: "Please pass a name on the query string or in the request body"
         };
-
-        context.done();
     }
 };
